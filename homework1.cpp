9a52789be50e5f89a18dc77f1bd95fdc8538a01b
hoganic <hoganic@bu.edu>
2016-09-14 19:40:05 -0400

    Main Function

---
+++ homework1.cpp
@@ -0,0 +1,26 @@
+// AUTHOR 1 Ian Hogan hoganic@bu.edu
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+//game main file
+int main()
+{
+	while(1==1){
+		int game = 0;
+		//ask user what game to play
+		cout << "Do you want to play game 1 or 2? " << endl;
+		cin >> game;
+	
+		if(game == 1){
+			//run game 1 (Ian's)
+		}
+		else if(game == 2){
+			//run game 2 (Hari's)
+		}
+		else
+		cout << "Invalid response" << endl;
+	}
+}
